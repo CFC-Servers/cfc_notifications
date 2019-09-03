40ff10dea27710cd2b14df92668b380e920a6b9b
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-09-03 02:24:24 -0400

    Initial structure

---
+++ lua/autorun/server/sv_cfc_notifications.lua
@@ -0,0 +1,44 @@
+util.AddNetworkString( "CFC_ReceivePopupNotification" )
+
+CFCNotifications = CFCNotifications or {}
+
+CFCNotifications._timerNameCount = 1
+
+local function getTimerName()
+    local newName = "notification-timer-" .. tostring( CFCNotifications._timerNameCount )
+
+    CFCNotifications._timerNameCount = CFCNotifications._timerNameCount + 1
+
+    return newName
+end
+
+local function makePopup( message, filter )
+    local getPlayers = filter or player.GetAll
+
+    local players = getPlayers()
+
+    net.Start( "CFC_PopupNotification" )
+        net.WriteString( message )
+    net.Send( players )
+end
+
+CFCNotifications._registerNotification = function( notificationType, message, delay, recurring, filter )
+    local repititions = recurring and 0 or 1
+
+    local callback = nil
+    if notificationType == "popup" then
+        callback = makePopup
+    end
+
+    if not callback then return "Invalid notification type" end
+
+    local timerName = getTimerName()
+    timer.Create( timerName, delay, repititions, function()
+        callback( message, filter )
+    end )
+end
+
+-- Public API Below
+CFCNotifications.registerPopup = function( message, interval, filter )
+    CFCNotifications._registerNotification( "popup", message, interval, true, filter )
+end
