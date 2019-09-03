d375cc169ca8e0d6979b591e0910ad8982a3a1f9
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-09-03 02:24:24 -0400

    Initial structure

---
+++ lua/autorun/client/cl_cfc_notifications.lua
@@ -0,0 +1,38 @@
+local function makePopupNotification( message )
+    local bubbleWidth = 300
+    local bubbleHeight = 170
+
+    local bubbleX = ScrW() - bubbleWidth
+    local bubbleY = ScrH() - ( bubbleHeight + 100 )
+
+    local Container = vgui.Create( "DPanel" )
+    Container:SetSize( bubbleWidth, bubbleHeight )
+    --Container:SetPos( containerX, containerY )
+    Container:SetDrawBackground( false )
+
+    local Bubble = vgui.Create( "DBubbleContainer", Container )
+    Bubble:SetBackgroundColor( Color( 36, 41, 67, 255 ) )
+
+    Bubble:OpenForPos( bubbleX, bubbleY, bubbleWidth, bubbleHeight )
+
+    local MessageLabel = vgui.Create( "DLabel", Bubble )
+    MessageLabel:SetPos( 5, 5 )
+    MessageLabel:SetSize( bubbleX * 0.9, bubbleY * 0.9 )
+    MessageLabel:SetWrap( true )
+    MessageLabel:SetFont( "Trebuchet24" )
+    MessageLabel:SetText( message )
+
+    function MessageLabel:PerformLayout()
+        Title:SetFGColor( Color( 255, 255, 255, 255 ) )
+        Title:SetToFullHeight()
+    end
+end
+
+net.Receive( "CFC_PopupNotification", function( len, ply )
+    local isFromServer = not ( IsValid( ply ) and ply:IsPlayer() )
+    if not isFromServer then return end
+
+    local message = net.ReadString()
+
+    makePopupNotification( message )
+end )
